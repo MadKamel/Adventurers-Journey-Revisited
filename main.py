8c68dcdcfc5f7025b5761636ce25ae1cbe91ebed
Eric Rustrum <rustrumek@gmail.com>
2021-01-20 14:31:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import actormanager
 
 ActorManager = actormanager.ActorManager()
+
+
+ActorManager.SpawnActor(2, 0, 0)
+ActorManager.SpawnActor(3, 0, 0)
+
+EnemiesInRoom = ActorManager.ScanAreaForEnemies(0, 0)
+while EnemiesInRoom != []:
+  for i in range(len(EnemiesInRoom)):
+    EnemiesInRoom[i].Hurt(1)
+    print('Attacked enemy ' + str(i))
+
+  for i in range(len(EnemiesInRoom)):
+    pass
