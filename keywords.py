1f1a689b9a441aa8bfea5f150659802291aaf7e5
Eric Rustrum <rustrumek@gmail.com>
2021-01-21 11:11:46 -0800

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,10 @@
+AttackEnemy = ['fight', 'attack', 'battle']
+
+AttackUp = ['up', 'top', 'above', 'high']
+AttackMid = ['mid', 'middle', 'center']
+AttackLow = ['down', 'bottom', 'below', 'low']
+
+MoveNorth = ['north', 'up', 'n', 'u']
+MoveSouth = ['south', 'down', 's', 'd']
+MoveEast = ['east', 'left', 'e', 'l']
+MoveWest = ['west', 'right', 'w', 'r']
