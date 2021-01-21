e347f720657b41135bbf6ab892da9b82c6b752e8
Eric Rustrum <rustrumek@gmail.com>
2021-01-21 11:11:21 -0800

    Update actormanager.py

--- actormanager.py
+++ actormanager.py
@@ -27,7 +27,7 @@ class ActorManager:
         Result.append(self.Actors[i])
     return Result
 
-  def ScanObjects(self, ActorType):
+  def ScanActors(self, ActorType):
     Result = []
     for i in range(len(self.Actors)):
       if self.Actors[i].ActorData[4] != 0 and self.Actors[i].ActorType == ActorType:
@@ -38,5 +38,12 @@ class ActorManager:
     Result = []
     for i in range(len(self.Actors)):
       if self.Actors[i].ActorData[2] == AreaX and self.Actors[i].ActorData[3] == AreaY and self.Actors[i].ActorData[4] == 1:
+        Result.append(self.Actors[i])
+    return Result
+
+  def ScanAreaForActors(self, AreaX, AreaY, ActorType):
+    Result = []
+    for i in range(len(self.Actors)):
+      if self.Actors[i].ActorData[2] == AreaX and self.Actors[i].ActorData[3] == AreaY and self.Actors[i].ActorData[4] == 1 and self.Actors[i].ActorType == ActorType:
         Result.append(self.Actors[i])
     return Result
